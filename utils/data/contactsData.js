7d318cc5ea75375f8c7b8b8d67e3dedee5da41ff
Tejas Soni <tejas.tejas.soni3@gmail.com>
2024-04-28 16:20:02 +0530

    added tejas soni portfolio using next js

---
+++ utils/data/contactsData.js
@@ -0,0 +1,10 @@
+export const contactsData = {
+    email: 'tejas.tejas.soni3@gmail.com',
+    phone: '+91 9662768548Shiv Tenament , Vadodara, Gujarat - 390021 ',
+    github: 'https://github.com/said7388',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "TEJAS SONI",
+  profile: '/profile.png',
+  designation: "Senior Software Engineer | Ex Laravel Team Leader | Technical Leader",
+  description: "Mame is Tejas Soni, a seasoned Senior Software Engineer with over 9 years of extensive experience in the field. I am deeply passionate about programming and thrive on the challenges it presents. Throughout my career, I've honed my skills in various technologies, specializing in PHP frameworks like Laravel and CodeIgniter, as well as GoLang for high-performance applications. With a strong foundation in database management, I've worked extensively with MySQL and MongoDB, ensuring efficient data storage and retrieval. JavaScript is at the heart of my development journey, and I leverage its power along with libraries like jQuery and frameworks like React to create dynamic and user-friendly web applications. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'tejas.tejas.soni3@gmail.com',
+  phone: '+91 9662768548',
+  address: 'Shiv Tenament , Vadodara, Gujarat - 390021 ',
+  github: 'https://github.com/tejassoni',
+  facebook: 'https://www.facebook.com/soni.teju/',
+  linkedIn: 'https://www.linkedin.com/in/soni-tejas/',
+  twitter: 'https://twitter.com/sonitejas',
+  stackOverflow: 'https://stackoverflow.com/users/3523478/tejas-soni',
+  leetcode: "https://leetcode.com/u/tejassoni/",
+  devUsername: "tejassoni",
+  resume: "https://drive.google.com/file/d/1zJ3Vezh734hVUHZ8KL89r2vMnBMnczc7/view?usp=sharing"
+}
